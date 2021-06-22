1a5ffd716f40dc4f2d3f79939ee567305bfb31f7
William Jesus <williamjesusti@gmail.com>
2021-06-22 20:33:56 -0300

    feat: create database and add migrations
    initialized database with sqlite and created the first table

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => res.json({ success: true, message: "Hello NLW!" }));
 
 app.listen(3333, () =>
   console.log("Server is running on http://localhost:3333")
