b1a9230bd119fa680ef01091d37462040a14844e
William Jesus <williamjesusti@gmail.com>
2021-06-22 20:37:04 -0300

    feat: created the first endpoint and entity
    add service and repository manager of entity User

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import express from "express";
 
+import { router } from "./routes";
 import "./database";
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.listen(3333, () =>
   console.log("Server is running on http://localhost:3333")
