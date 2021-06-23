9f9d6ed1c35cbc28906c2ef561daf5340ddead08
William Jesus <williamjesusti@gmail.com>
2021-06-22 21:22:39 -0300

    fix: add exception control

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import "reflect-metadata";
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Response, Request } from "express";
 
 import { router } from "./routes";
+
 import "./database";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({ status: "error", error: err.message });
+  }
+
+  return res
+    .status(500)
+    .json({ status: "error", error: "internal server error" });
+});
 
 app.listen(3333, () =>
   console.log("Server is running on http://localhost:3333")
